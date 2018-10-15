592d7f2bb4b01461db3890acdd976583f3523960
David Michelson <dmichels@usc.edu>
2018-10-14 23:30:50 -0700

    added mainactivity and set to default activity

--- app/src/dbhelpers/java/dbhelpers/DatabaseHelper.java
+++ app/src/dbhelpers/java/dbhelpers/DatabaseHelper.java
@@ -6,26 +6,26 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
+    private static final String DATABASE_NAME ="KillDDL_DB"
     private static final int DATABASE_VERSION = 1;
 
     // task table
-    public static final String CREATE_TASK_TABLE = "";
-
-    // task list table
-
+    public static final String CREATE_TASKS_TABLE = "CREATE TABLE IF NOT EXISTS tasks" +
+            "(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT)";
+    
 
 
     public DatabaseHelper(Context context) {
-
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        // sqLiteDatabase.execSQL();
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL(CREATE_TASKS_TABLE);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
 
     }
 
