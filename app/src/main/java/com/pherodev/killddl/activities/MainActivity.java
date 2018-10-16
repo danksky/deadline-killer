71f42b907e633dc3579c039baf90725fcf2064d5
David Michelson <dmichels@usc.edu>
2018-10-16 00:17:31 -0700

    playing around with recyclerview for tasklists in mainactivity for now

--- app/src/main/java/com/pherodev/killddl/activities/MainActivity.java
+++ app/src/main/java/com/pherodev/killddl/activities/MainActivity.java
@@ -2,14 +2,23 @@ package com.pherodev.killddl.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 import com.pherodev.killddl.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    // RecyclerView https://developer.android.com/guide/topics/ui/layout/recyclerview
+    private RecyclerView taskLists;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadTaskLists();
+    }
+
+    private void loadTaskLists() {
+
     }
 }
