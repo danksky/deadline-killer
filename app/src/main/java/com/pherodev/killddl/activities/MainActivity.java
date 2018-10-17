a4e45c33694d33a2e72b38084ec8f5433fa3a3ec
David Michelson <dmichels@usc.edu>
2018-10-17 10:08:09 -0700

    added more recyclerView stuff to mainactivity

--- app/src/main/java/com/pherodev/killddl/activities/MainActivity.java
+++ app/src/main/java/com/pherodev/killddl/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pherodev.killddl.activities;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
@@ -9,7 +10,11 @@ import com.pherodev.killddl.R;
 public class MainActivity extends AppCompatActivity {
 
     // RecyclerView https://developer.android.com/guide/topics/ui/layout/recyclerview
-    private RecyclerView taskLists;
+    private RecyclerView taskListsRecyclerView;
+    private RecyclerView.Adapter taskListsAdapter;
+    private RecyclerView.LayoutManager taskListsLayoutManager;
+
+    private FloatingActionButton createTaskListFloatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
