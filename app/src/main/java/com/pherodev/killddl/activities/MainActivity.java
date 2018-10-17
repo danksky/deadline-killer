614a5a14823e95e55f53ab8811c7235c01346efe
David Michelson <dmichels@usc.edu>
2018-10-17 10:50:45 -0700

    created card for task_lists

--- app/src/main/java/com/pherodev/killddl/activities/MainActivity.java
+++ app/src/main/java/com/pherodev/killddl/activities/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Task Lists");
         setContentView(R.layout.activity_main);
         loadTaskLists();
     }
