3ef1c2f5ff1926265e87b1a883120f20b37d0585
David Michelson <dmichels@usc.edu>
2018-10-17 11:15:39 -0700

    added taskList model to DatabaseHelper

--- app/src/dbhelpers/java/dbhelpers/DatabaseHelper.java
+++ app/src/dbhelpers/java/dbhelpers/DatabaseHelper.java
@@ -9,11 +9,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME ="KillDDL_DB";
     private static final int DATABASE_VERSION = 1;
 
-    // task table
-    public static final String CREATE_TASKS_TABLE = "CREATE TABLE IF NOT EXISTS tasks" +
-            "(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT)";
+    // tasks table
+    public static final String CREATE_TASKS_TABLE = "";
 
-
+    // taskLists table
+    public static final String CREATE_TASKLISTS_TABLE = "CREATE TABLE IF NOT EXISTS taskLists" +
+            "(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT" +
+            ",title TEXT NOT NULL)";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -22,11 +24,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase database) {
         database.execSQL(CREATE_TASKS_TABLE);
+        database.execSQL(CREATE_TASKLISTS_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
-
+        database.execSQL("DROP TABLE IF EXISTS taskLists");
+        onCreate(database);
     }
 
 }
