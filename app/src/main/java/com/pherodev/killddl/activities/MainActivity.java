30b55a574cee778736a185f2afb5d93e864e55bd
David Michelson <dmichels@usc.edu>
2018-10-17 11:07:55 -0700

    building out task list activity with data binding

--- app/src/main/java/com/pherodev/killddl/activities/MainActivity.java
+++ app/src/main/java/com/pherodev/killddl/activities/MainActivity.java
@@ -1,11 +1,13 @@
 package com.pherodev.killddl.activities;
 
+import android.databinding.DataBindingUtil;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 
 import com.pherodev.killddl.R;
+import com.pherodev.killddl.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView taskListsRecyclerView;
     private RecyclerView.Adapter taskListsAdapter;
     private RecyclerView.LayoutManager taskListsLayoutManager;
+    private ActivityMainBinding mActivityMainBinding;
 
     private FloatingActionButton createTaskListFloatingActionButton;
 
@@ -20,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Task Lists");
+        mActivityMainBinding = DataBindingUtil.setContentView(
+                this, R.layout.activity_main);
+
+        // now get list of taskLists with SQLite
+
         setContentView(R.layout.activity_main);
         loadTaskLists();
     }
