a11df355d4e47adc6f9a4d97b2661c62215bb226
David Michelson <dmichels@usc.edu>
2018-10-19 17:54:09 -0700

    clicking on a category opens tasks activity and passes category ID

--- app/src/main/java/com/pherodev/killddl/adapters/CategoryAdapter.java
+++ app/src/main/java/com/pherodev/killddl/adapters/CategoryAdapter.java
@@ -1,5 +1,6 @@
 package com.pherodev.killddl.adapters;
 
+import android.content.Intent;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
@@ -9,6 +10,8 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.pherodev.killddl.R;
+import com.pherodev.killddl.activities.CategoryInputActivity;
+import com.pherodev.killddl.activities.TasksActivity;
 import com.pherodev.killddl.models.Category;
 
 import java.util.ArrayList;
@@ -36,6 +39,7 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
     @Override
     public void onBindViewHolder(CategoryViewHolder categoryViewHolder, int i) {
         categoryViewHolder.categoryTitle.setText(categories.get(i).getTitle());
+        categoryViewHolder.categoryId = categories.get(i).getId();
     }
 
     @Override
@@ -46,14 +50,20 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
     public static class CategoryViewHolder extends RecyclerView.ViewHolder {
         CardView cv;
         TextView categoryTitle;
+        int categoryId;
 
         CategoryViewHolder(View itemView) {
             super(itemView);
             cv = (CardView)itemView.findViewById(R.id.cv);
             cv.setOnClickListener(new View.OnClickListener() {
                 @Override public void onClick(View v) {
-                    Snackbar.make(v, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                    // Based on which item was clicked, we want to open the Tasks activity with that ID
+                    // Maybe we do this in OnCreateViewHolder?
+                    Snackbar.make(v, "Clicked on category with iD: " + categoryId, Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
+                    Intent intent = new Intent (v.getContext(), TasksActivity.class);
+                    intent.putExtra("CATEGORY_ID", categoryId);
+                    v.getContext().startActivity(intent);
                 }
             }) ;
             categoryTitle = (TextView)itemView.findViewById(R.id.category_title);
