4caca404591667043010ed9f8a7c3c71c04c3629
drakerehfeld <drakerehfeld@gmail.com>
2018-10-28 16:13:45 -0700

    drake 4 tests

--- app/src/test/java/com/pherodev/killddl/CategoryTests.java
+++ app/src/test/java/com/pherodev/killddl/CategoryTests.java
@@ -6,15 +6,39 @@ import com.pherodev.killddl.models.Category;
 
 public class CategoryTests {
     @Test
-    public void testCategoryModelGetters() {
+    public void createCategoryUsingConstructor() {
         String title = "testing title";
         int ide = 123;
         Category c = new Category(ide,title);
         assertTrue(c.getTitle().equals(title));
         assertTrue(c.getId() == ide);
     }
+
     @Test
-    public void testCategoryModelSetters() {
+    public void editCategoryData() {
+        String title = "testing title";
+        int ide = 123;
+        Category c = new Category(ide,title);
+        String newTitle = "new title";
+        c.setTitle(newTitle);
+        assertTrue(c.getTitle().equals(newTitle));
+        assertTrue(c.getId() == ide);
+    }
+
+
+    @Test
+    public void editCategoryId() {
+        String title = "testing title";
+        int ide = 123;
+        Category c = new Category(ide,title);
+        int ideNew = 1234;
+        c.setId(ideNew);
+        assertTrue(c.getTitle().equals(title));
+        assertTrue(c.getId() == ideNew);
+    }
+
+    @Test
+    public void getCategoryData() {
         String title = "testing title";
         int ide = 123;
         Category c = new Category();
