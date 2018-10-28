5e5c068e0b7a0d8db16ef35adfe0fca064b4fd37
Daniel Kawalsky <dkawalsk@usc.edu>
2018-10-28 14:05:29 -0700

    CategoryActivityTests complete

--- app/src/androidTest/java/com/pherodev/killddl/CategoryActivityTest.java
+++ app/src/androidTest/java/com/pherodev/killddl/CategoryActivityTest.java
@@ -1,13 +1,18 @@
 package com.pherodev.killddl;
 
 import android.content.Context;
+import android.content.Intent;
 
 import com.pherodev.killddl.activities.CategoryActivity;
+import com.pherodev.killddl.activities.TasksActivity;
 import com.pherodev.killddl.models.Category;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.UUID;
 
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.filters.LargeTest;
@@ -15,8 +20,10 @@ import androidx.test.runner.AndroidJUnit4;
 
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -24,6 +31,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 
 import static org.junit.Assert.*;
 
@@ -39,6 +47,7 @@ public class CategoryActivityTest {
     @Rule
     public ActivityTestRule<CategoryActivity> categoryActivityRule =
             new ActivityTestRule<>(CategoryActivity.class);
+    public String uniqueCategoryTitle = UUID.randomUUID().toString();
 
     @Test
     public void useAppContext() {
@@ -48,25 +57,65 @@ public class CategoryActivityTest {
     }
 
     @Test
-    public void floatingActionButtonExists() {
-        onView(withId(R.id.fab_create_category)).check(matches(isDisplayed()));
-    }
-
-    @Test
-    public void floatingActionButtonClickLaunchesInput() {
-        onView(withId(R.id.fab_create_category)).perform(click());
-        onView(withId(R.id.edit_text_input_category_title)).check(matches(isDisplayed()));
-        onView(withId(R.id.edit_text_input_task_title)).check(doesNotExist());
-    }
-
-    @Test
     public void createCategoryItem() {
         onView(withId(R.id.fab_create_category)).perform(click());
-        onView(withId(R.id.edit_text_input_category_title)).perform(typeText("Title 1"), closeSoftKeyboard());
+        onView(withId(R.id.edit_text_input_category_title)).perform(typeText(uniqueCategoryTitle), closeSoftKeyboard());
         onView(withId(R.id.fab_input_category_complete)).check(matches(isDisplayed()));
         onView(withId(R.id.fab_input_category_complete)).perform(click());
         onView(withId(R.id.fab_input_category_complete)).check(doesNotExist());
-        onView(withId(R.id.recycler_view_categories)).check(matches(hasDescendant(withText("Title 1"))));
+        onView(withId(R.id.recycler_view_categories)).check(matches(hasDescendant(withText(uniqueCategoryTitle))));
+    }
+
+    @Test
+    public void emptyCategoryItem() {
+        onView(withId(R.id.fab_create_category)).perform(click());
+        onView(withId(R.id.edit_text_input_category_title)).perform(typeText(""), closeSoftKeyboard());
+        onView(withId(R.id.fab_input_category_complete)).check(matches(isDisplayed()));
+        onView(withId(R.id.fab_input_category_complete)).perform(click());
+        // Rather than check if the error message appears, check that your activity hasn't finished
+        onView(withId(R.id.fab_input_category_complete)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void excessiveTitleCategoryItem() {
+        onView(withId(R.id.fab_create_category)).perform(click());
+        StringBuilder excessiveLengthSB = new StringBuilder();
+        for (int i = 0; i < 5; i++)
+            excessiveLengthSB.append("qwertyuiopasdfghjklzxcvbnm");
+        onView(withId(R.id.edit_text_input_category_title)).perform(typeText(excessiveLengthSB.toString()), closeSoftKeyboard());
+        onView(withId(R.id.fab_input_category_complete)).check(matches(isDisplayed()));
+        onView(withId(R.id.fab_input_category_complete)).perform(click());
+        // Rather than check if the error message appears, check that your activity hasn't finished
+        onView(withId(R.id.fab_input_category_complete)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void deleteCategoryItem() {
+        categoryActivityRule.getActivity().programmaticallyDeleteLastCategoryEntry();
+        onView(withText(uniqueCategoryTitle)).check(doesNotExist());
+    }
+
+    // Must be run after deleteCategoryItem()
+    @Test
+    public void userCancelsCreateCategoryItem() {
+        onView(withId(R.id.fab_create_category)).perform(click());
+        onView(withId(R.id.edit_text_input_category_title)).perform(typeText(uniqueCategoryTitle), closeSoftKeyboard());
+        pressBack();
+        // Otherwise this fails
+        onView(withText(uniqueCategoryTitle)).check(doesNotExist());
+    }
+
+    @Test
+    public void launchTasksActivity() {
+        // Create a Category through UI to reach TasksActivity
+        onView(withId(R.id.fab_create_category)).perform(click());
+        onView(withId(R.id.edit_text_input_category_title)).perform(typeText(uniqueCategoryTitle), closeSoftKeyboard());
+        onView(withId(R.id.fab_input_category_complete)).perform(click());
+        categoryActivityRule.getActivity().programmaticallyLaunchTasksActivityWithLatestCategory();
+        onView(withId(R.id.fab_create_task)).check(matches(isDisplayed()));
+        // This is going to clutter up CategoryActivity without delete
+        categoryActivityRule.getActivity().programmaticallyDeleteLastCategoryEntry();
+        onView(withText(uniqueCategoryTitle)).check(doesNotExist());
     }
 
 }
