9eb3470153b34261e25f07c7de873d66b4e87dcc
Adam Liber <aliber@usc.edu>
2018-10-28 19:52:32 -0700

    added dbhelper white-box unit tests: deleteCategory, deleteTask, selectTasksByCategory

--- app/src/test/java/com/pherodev/killddl/DatabaseHelperTests.java
+++ app/src/test/java/com/pherodev/killddl/DatabaseHelperTests.java
@@ -18,6 +18,8 @@ import static junit.framework.Assert.assertEquals;
 
 @RunWith(RobolectricTestRunner.class)
 public class DatabaseHelperTests {
+
+
     @Test
     public void testCreatesCategory() {
         String title = "testCategoryDBTitle";
@@ -39,6 +41,41 @@ public class DatabaseHelperTests {
         assertEquals(title,dbTitle);
         dbHelper.close();
 
+    }
+    @Test
+    public void deleteCategory(){
+        String title = "testDeleteCategoryDBTitle";
+        CategoryInputActivity activity = Robolectric.setupActivity(CategoryInputActivity.class);
+        DatabaseHelper dbHelper = new DatabaseHelper(activity);
+        long newRowId = dbHelper.createCategory( title);
+        String[] args = {Long.toString(newRowId)};
+
+        Cursor cursor  =  dbHelper.getWritableDatabase().rawQuery(DatabaseHelper.Category.SELECT_CATEGORY_NAME_BY_ID,args);
+        String dbTitle = "";
+        try {
+            while (cursor.moveToNext()) {
+                dbTitle = cursor.getString(cursor.getColumnIndex(DatabaseHelper.Category.COLUMN_TITLE));
+            }
+        } finally {
+            cursor.close();
+        }
+
+        assertEquals(title,dbTitle);
+
+        dbHelper.deleteCategory(newRowId);
+        cursor  =  dbHelper.getWritableDatabase().rawQuery(DatabaseHelper.Category.SELECT_CATEGORY_NAME_BY_ID,args);
+        dbTitle = "";
+        try {
+            while (cursor.moveToNext()) {
+                dbTitle = cursor.getString(cursor.getColumnIndex(DatabaseHelper.Category.COLUMN_TITLE));
+            }
+        } finally {
+            cursor.close();
+        }
+
+        assertEquals("",dbTitle);
+
+        dbHelper.close();
     }
 
     @Test
@@ -107,6 +144,79 @@ public class DatabaseHelperTests {
         dbHelper.close();
     }
 
+    @Test
+    public void testDeleteTask(){
+        String categoryTitle = "testDeleteTaskDBEdit";
+        String originalTitle = "testDeleteTaskDBTitle";
+
+        TaskInputActivity activity = Robolectric.setupActivity(TaskInputActivity.class);
+        DatabaseHelper dbHelper = new DatabaseHelper(activity);
+        long catId = dbHelper.createCategory(categoryTitle);
+        long taskId = dbHelper.createTask(catId,originalTitle,"testEditTask","10/27/18" );
+        String[] args = {Long.toString(catId)};
+        Cursor cursor  =  dbHelper.getWritableDatabase().rawQuery(DatabaseHelper.Task.SELECT_TASK_BY_ID,args);
+
+
+        String dbOriginalTaskTitle = "";
+        try {
+            while (cursor.moveToNext()) {
+                dbOriginalTaskTitle = cursor.getString(cursor.getColumnIndex(DatabaseHelper.Task.COLUMN_TITLE));
+            }
+        }finally {
+            cursor.close();
+        }
+
+        assertEquals(originalTitle,dbOriginalTaskTitle);
+
+        dbHelper.deleteTask(taskId);
+        cursor  =  dbHelper.getWritableDatabase().rawQuery(DatabaseHelper.Task.SELECT_TASK_BY_ID,args);
+        String dbTaskTitle = "";
+        try {
+            while (cursor.moveToNext()) {
+                dbTaskTitle = cursor.getString(cursor.getColumnIndex(DatabaseHelper.Task.COLUMN_TITLE));
+            }
+        }finally {
+            cursor.close();
+        }
+
+        assertEquals("",dbTaskTitle);
+
+        dbHelper.close();
+    }
+
+    @Test
+    public void testSelectTasksByCategory(){
+        String categoryTitle = "testSelectTaskDBEdit";
+        String otherCategoryTitle = "testSelectTaskDBDecoy";
+        String taskTitle = "testSelectTaskDBTitle";
+
+        TaskInputActivity activity = Robolectric.setupActivity(TaskInputActivity.class);
+        DatabaseHelper dbHelper = new DatabaseHelper(activity);
+
+        long catId = dbHelper.createCategory(categoryTitle);
+        long catIdDecoy = dbHelper.createCategory(otherCategoryTitle);
+
+        // task that should be included in db selection
+        dbHelper.createTask(catId,taskTitle,"testSelect","10/27/18" );
+        // task that should not be includec in db selection
+        dbHelper.createTask(catIdDecoy,taskTitle,"testSelectDecoy","10/27/18" );
+
+        String[] args = {Long.toString(catId)};
+        Cursor cursor  =  dbHelper.selectTaskFromCategory(catId);
+
+        try {
+            // all rows should selected should be from category with id: catId 
+            while (cursor.moveToNext()) {
+                Long dbTaskCategoryId = cursor.getLong(cursor.getColumnIndex(DatabaseHelper.Task.COLUMN_CATEGORY_ID));
+                assertEquals( (Long) catId, (Long) dbTaskCategoryId);
+            }
+        }finally {
+            cursor.close();
+        }
+
+
+        dbHelper.close();
+    }
 
 
 
